c471ca622a1359d5e737c559191b6db43a441af4
betty <makewithbetty@gmail.com>
2018-08-24 05:11:32 -0700

    Style and add basic interactivity

---
+++ js/main.js
@@ -0,0 +1,52 @@
+document.getElementById('btn-menu').addEventListener('click', function() {
+  document.getElementById('nav').classList.toggle('show');
+});
+
+document.getElementById('btn-scroll').addEventListener('click', function(e) {
+
+  var aboutOffsetTop = document.getElementById('about').offsetTop;
+  var scrollPosition = window.pageYOffset;
+
+  var scrollFn = setInterval(function() {
+    scrollPosition += 10;
+    window.scrollTo(0, scrollPosition)
+    if (window.pageYOffset >= aboutOffsetTop) {
+      clearInterval(scrollFn);
+    }
+  }, 0);
+});
+
+var productGalleryPics = [
+  {
+    imageUrl: 'images/products-mug.jpg',
+    alt: 'Gudetama mug'
+  },
+  {
+    imageUrl: 'images/products-cellcase.jpg',
+    alt: 'Gudetama cellphone case'
+  },
+  {
+    imageUrl: 'images/products-bag.jpg',
+    alt: 'Gudetama bag'
+  },
+  {
+    imageUrl: 'images/products-keychains.jpg',
+    alt: 'Gudetama keychains'
+  },
+];
+
+var productPicIndex = 0;
+
+document.getElementById('btn-next').addEventListener('click', function() {
+  var productPic = document.getElementById('product-pic');
+  productPicIndex++;
+  
+  if(productPicIndex >= productGalleryPics.length) {
+    productPicIndex = 0;
+  }
+
+  var nextProduct = productGalleryPics[productPicIndex];
+
+  productPic.src = nextProduct.imageUrl;
+  productPic.alt = nextProduct.alt;
+});
